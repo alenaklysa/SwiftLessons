3dfe0eff7d9322b1e183425cb564baa9d4df3d58
alenaklysa <alena.klysa0503@gmail.com>
2019-12-11 19:43:11 +0200

    added calculator

--- Swift lessons Alena/lesson8/homework/CaclViewControler.swift
+++ Swift lessons Alena/lesson8/homework/CaclViewControler.swift
@@ -9,9 +9,16 @@
 import Foundation
 import UIKit
 
+enum mark : String {
+    case plus = "+"
+     case minus = "-"
+     case multipl = "*"
+     case division = "/"
+}
 class CaclViewControler: UIViewController {
     // MARK props
-    
+    var num1 : Float! = 0
+    var calc : Float! = 0
     
     // MARK IBOutlet
     @IBOutlet weak var zeroButton: UIButton!
@@ -35,47 +42,95 @@ class CaclViewControler: UIViewController {
     
     // MARK IBAction
     
-    
+    @IBAction func zeroButtonAction(sender: UIButton) {
+         getResult.text = ( getResult.text ?? String()) + "0"
+           num1 = Float (getResult.text!)
+           
+       }
     @IBAction func oneButtonAction(sender: UIButton) {
       getResult.text = ( getResult.text ?? String()) + "1"
-      }
+        num1 = Float (getResult.text!)
+        
+    }
       @IBAction func twoButtonAction(sender: UIButton) {
            getResult.text = ( getResult.text ?? String()) + "2"
+        num1 = Float (getResult.text!)
     }
     @IBAction func threeButtonAction(sender: UIButton) {
         getResult.text = ( getResult.text ?? String()) + "3"
+     num1 = Float (getResult.text!)
     }
     @IBAction func fourButtonAction(sender: UIButton) {
       getResult.text = ( getResult.text ?? String()) + "4"
+    num1 = Float (getResult.text!)
     }
     @IBAction func fiveButtonAction(sender: UIButton) {
         getResult.text = ( getResult.text ?? String()) + "5"
+        num1 = Float (getResult.text!)
     }
     @IBAction func sixButtonAction(sender: UIButton) {
          getResult.text = ( getResult.text ?? String()) + "6"
+         num1 = Float (getResult.text!)
     }
     @IBAction func sevenButtonAction(sender: UIButton) {
       getResult.text = ( getResult.text ?? String()) + "7"
+        num1 = Float (getResult.text!)
         
     }
     @IBAction func eightButtonAction(sender: UIButton) {
        getResult.text = ( getResult.text ?? String()) + "8"
+        num1 = Float (getResult.text!)
     }
     @IBAction func nineButtonAction(sender: UIButton) {
          getResult.text = (getResult.text ?? String()) + "9"
+         num1 = Float (getResult.text!)
     }
     
     @IBAction func getResultButtonAction(sender: UIButton) {
-        let calc1 = getResult.text!
-        let getResult: Float = Float (calc1)!
-        let plus: Float = getResult  
-        
-        
+       calc = Float (getResult.text!)!
+        if sender.tag != 5{
+            if sender.tag == 1 {
+            getResult.text =  "+" +
+           
+                
+        }
+        else if sender.tag == 2
+         {
+            getResult.text =  (getResult.text ?? String()) + "-"
+           
+        }
+        else if sender.tag == 3
+         {
+            getResult.text = (getResult.text ?? String()) +  "*"
+            
+        }
+        else if sender.tag == 4
+         {
+            getResult.text =  (getResult.text ?? String()) + "/"
+           
+            }}
+       
+         else if sender.tag == 5 {
+            
+            if sender.tag == 1 {
+                getResult.text = String (num1 + calc )
+            }
+            else if sender.tag == 2 {
+                getResult.text =  String (num1 - calc )
+            }
+            else if sender.tag == 3 {
+                getResult.text =  String (num1 * calc )
+            }
+            else if sender.tag == 4 {
+                getResult.text =  String (num1 / calc )
+            }
+        }
     }
-    func selectButton(sender: UIButton) {
+        
     
-        sender.isSelected = !sender.isSelected
-        
-    }
     
 }
+
+
+    
+
